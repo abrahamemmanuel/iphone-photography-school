7baa4fe90f40c96d269f1cf42c68015ebf40e358
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2023-09-10 23:40:10 +0100

    added badge service and interface

---
+++ app/Services/Badge.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Interfaces\Badge;
+
+class BadgeService implements Badge
+{
+    public $badge int
+
+    public function __construct()
+    {
+        // 
+    }
+    public function beginner(): int
+    {
+        return $badge;
+    }
+
+    public function intermediate(): int
+    {
+        return $badge;
+    }
+
+    public function advanced(): int
+    {
+        return $badge;
+    }
+
+    public function master(): int
+    {
+        return $badge;
+    }
+}
