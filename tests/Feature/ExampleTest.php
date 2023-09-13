685263c3283ed286413d7fffa4ebbbf19685fe21
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2023-09-13 09:40:21 +0100

    written first test for user achievement

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,16 +16,33 @@ class ExampleTest extends TestCase
 {
     use RefreshDatabase;
     /**
+     * test
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_that_user_achieves_first_comment_written()
     {
         $this->withoutExceptionHandling();
         $user = User::factory()->create();
-        $this->commentWriter(15, $user);
+        $this->commentWriter(1, $user);
         $response = $this->get("/users/{$user->id}/achievements");
-        
-        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'unlocked_achievements' => [
+                'First Comment Written Achievements'
+            ],
+            'next_available_achievements' => [
+                '3 Comments Written Achievements'
+            ],
+            'current_badge' => 'Beginner',
+            'next_badge' => 'Intermediate',
+            'remaing_to_unlock_next_badge' => 3,
+        ]);
+        $this->assertCount(1, $response['unlocked_achievements']);
+        $this->assertCount(1, $response['next_available_achievements']);
+        $this->assertIsArray($response['unlocked_achievements']);
+        $this->assertIsArray($response['next_available_achievements']);
+        $this->assertIsString($response['current_badge']);
+        $this->assertIsString($response['next_badge']);
+        $this->assertIsInt($response['remaing_to_unlock_next_badge']);
     }
 
     public function commentWriter($number, $user)
