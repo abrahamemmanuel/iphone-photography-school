5825fc51ae3a854cb4d9987961159eb632114e2e
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2023-09-14 11:00:51 +0100

    added watched to user model class

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,12 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const FIRST_LESSON_WATCHED_ACHIEVEMENT = 'First Lesson Watched';
+    const FIVE_LESSONS_WATCHED_ACHIEVEMENT = '5 Lessons Watched';
+    const TEN_LESSONS_WATCHED_ACHIEVEMENT = '10 Lessons Watched';
+    const TWENTY_FIVE_LESSONS_WATCHED_ACHIEVEMENT = '25 Lessons Watched';
+    const FIFTY_LESSONS_WATCHED_ACHIEVEMENT = '50 Lessons Watched';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -49,5 +55,10 @@ class User extends Authenticatable
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function watched()
+    {
+        return $this->hasMany(Lesson::class, 'lesson_user', 'user_id', 'lesson_id');
     }
 }
